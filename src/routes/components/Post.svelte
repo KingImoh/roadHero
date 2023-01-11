834d915bf4f7909bb3bb6fa3f03418e8ae237d1a
Imoh Gbinije <princeemmanuel05@gmail.com>
2023-01-11 04:56:22 +0100

    :lipstick: UI update to the Post Card

--- src/routes/components/Post.svelte
+++ src/routes/components/Post.svelte
@@ -10,10 +10,14 @@
   import "swiper/css/navigation";
   import "swiper/css/pagination";
   import "swiper/css/scrollbar";
+  import clsx from "clsx";
+
+  let liked = false;
+  let commented = false;
 </script>
 
 <div
-  class="w-full min-h-450px max-h-550px rounded-3xl border p-3 shadow mb-6 flex flex-col justify-evenly bg-white"
+  class="w-full min-h-450px max-h-550px rounded-3xl border p-4 shadow mb-6 flex flex-col justify-evenly bg-white"
 >
   <div class="h-20% flex items-center">
     <div class="profilepic rounded-full w-15 h-15 bg-black " />
@@ -38,7 +42,7 @@
     </div>
 
     <!-- Time Stamp -->
-    <div class="text-grey">4 mins ago</div>
+    <div class="text-[#0B2040] opacity-50 text-xs">4 mins ago</div>
 
     <!-- content wrapper -->
     <div class="rounded-3xl w-full h-full overflow-hidden max-h-175px h-1/2">
@@ -62,30 +66,58 @@
 
   <!-- Card Footer -->
   <!-- has the likes(i.e appreciate), comments, etc -->
-  <div class="h-15% flex space-x-2 text-sm items-center">
-    <!-- Appreciate -->
+  <div class="h-15% flex space-x-6 text-lg items-center pt-4 text-[#0B2040]">
     <div
-      class="bg-primaryBlue p-2 w-80px h-40px rounded-full text-white flex items-center justify-evenly"
+      class={clsx(liked && "text-secondaryGreen")}
+      on:click={() => {
+        liked = !liked;
+      }}
+      on:keydown={() => {}}
     >
-      <div class="i-emojione-monotone-clapping-hands" />
-      <div>450</div>
+      {#if liked != true}
+        <div class="i-icon-park-outline-thumbs-up" />
+      {:else}
+        <div class="i-ion-md-thumbs-up" />
+      {/if}
     </div>
 
-    <!-- Comments -->
     <div
-      class="bg-primaryBlue p-2 w-80px h-40px rounded-full text-white flex items-center justify-evenly"
+      class={clsx(commented && "text-secondaryGreen")}
+      on:click={() => {
+        commented = !commented;
+      }}
+      on:keydown={() => {}}
     >
-      <div class="i-material-symbols-comment-outline-rounded" />
-      <div>50</div>
+      <div class="i-material-symbols-comment-outline-rounded  " />
     </div>
 
-    <!-- Share -->
-    <div
-      class="bg-grey p-2 px-4 h-40px rounded-full text-primaryBlue flex items-center justify-evenly"
-    >
-      <div class="i-ion-share-social-outline" />
-    </div>
+    <div class="i-ion-share-social-outline" />
   </div>
+
+  <!-- </div> -->
+
+  <div class="pt-3 flex justify-between text-xs">
+    <div class=" ">2,340 <span class="text-xs">thumbs-up</span></div>
+    <div class=" ">340 <span class="text-xs">comments</span></div>
+  </div>
+
+  <!-- Comment section -->
+  {#if commented}
+    <div class="w-full my-4 mx-auto flex items-center">
+      <div class="h-1px opacity-50 bg-[#E8E8E8] w-35%" />
+      <div class="text-xs text-[#E8E8E8]">view comments</div>
+      <div class="h-1px opacity-50 bg-[#E8E8E8] w-35%" />
+    </div>
+
+    <div class="bg-grey w-full p-3 rounded-lg flex justify-between">
+      <input type="text" class="bg-transparent w-70% outline-none " placeholder="Comment" />
+      <div
+        class="square-30px rounded-full bg-secondaryGreen flex justify-center items-center text-white"
+      >
+        <div class="i-mingcute-send-plane-line" />
+      </div>
+    </div>
+  {/if}
 </div>
 
 <style>
